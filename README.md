76a07a79f4169a808c4eb80b0a4c57353557809b
Jaime <jaimeloeuf@gmail.com>
2020-09-01 19:13:12 +0800

    Initial Commit:     Scaffolded with https://github.com/Enkel-Digital/vue-bulma-template.

---
+++ README.md
@@ -0,0 +1,33 @@
+# solutions-bulma
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## License, Author and Contributing
+This project is developed and made available under the "AGPL License"  
+If you have any questions, contact us via [email](mailto:developer@enkeldigital.com)  
+Authors:
+- [JJ](https://github.com/Jaimeloeuf)
+- [Jessica](https://github.com/jessicajacelyn)
+- [Zi Kang](https://github.com/zzkzzzz)
+- [Joshua](https://github.com/leeszeray)
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <section id="home" class="hero is-fullheight">
+      <img :src="heroImage" alt="Hero" class="image hero-image" />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return {
+      heroImage:
+        "https://images.unsplash.com/39/lIZrwvbeRuuzqOoWJUEn_Photoaday_CSD%20(1%20of%201)-5.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    };
+  },
+};
+</script>
+
+// Load the bulma styles in the top level component
+<style lang="sass" src="bulma" />
+
+<style lang="css">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.hero-image {
+  overflow: hidden;
+  position: absolute;
+  object-fit: cover;
+  object-position: center center;
+  width: 100%;
+  height: 100%;
+}
+</style>
