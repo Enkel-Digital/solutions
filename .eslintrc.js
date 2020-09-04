a0042189f6dbb4fa6b87606a8c2ab875cb47752c
Jaime <jaimeloeuf@gmail.com>
2020-09-05 00:56:53 +0800

    Optimize with pre-rendering build step:     Adds pre-rendering build step to generate a static site to optimize     for faster rendering on clients' devices.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
   rules: {
     "no-unused-vars": errorOnProduction,
     "no-unused-components": errorOnProduction,
-    "no-console": errorOnProduction,
     "no-debugger": errorOnProduction,
     "comma-dangle": ["error", "only-multiline"],
   },
--- src/main.js
+++ src/main.js
@@ -8,4 +8,8 @@ Vue.config.productionTip = false;
 new Vue({
   // router,
   render: (h) => h(App),
+  mounted() {
+    // This is needed for "prerender-spa-plugin"'s renderAfterDocumentEvent to take snapshot of static content once everything is rendered.
+    document.dispatchEvent(new Event("vue-render-complete"));
+  },
 }).$mount("#app");
