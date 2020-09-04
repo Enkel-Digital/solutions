1de691aec8851138a2e8125f1c511788efc195bb
Jaime <jaimeloeuf@gmail.com>
2020-09-04 19:28:13 +0800

    WIP TechStack component.

---
+++ src/components/TechStack.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="section">
+    <div class="columns is-centered">
+      <div class="column is-two-thirds">
+        <h1 class="title">Our solid and versatile technology stack</h1>
+
+        <div class="tile is-ancestor">
+          <div class="tile is-6 is-vertical is-parent">
+            <div class="tile is-child box has-text-left">
+              <p class="title">
+                <i class="fas fa-layer-group" /> Cloud / Azure
+              </p>
+              <p>Cloud native applications.</p>
+            </div>
+          </div>
+
+          <div class="tile is-6 is-vertical is-parent">
+            <div class="tile is-child box has-text-left">
+              <p class="title"><i class="fas fa-chart-area" /> Gcp</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                ornare magna eros, eu pellentesque tortor vestibulum ut.
+                Maecenas non massa sem. Etiam finibus odio quis feugiat
+                facilisis.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TechStack",
+};
+</script>
