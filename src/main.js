7269bbcad4fbb5a7e68b808110964f806ebd35e8
Jaime <jaimeloeuf@gmail.com>
2020-09-06 01:27:51 +0800

    Adds setup code to redirect if needed:     Setup IIFE redirects user to the correct URL if needed, when the site     is accessed via the firebase provisioned URL that cannot be disabled.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+// Function to redirect user to the preferred domain instead of the default firebase hosting domains if they land there.
+// IIFE to prevent polluting the global namespace accidentally
+(function () {
+  // Short circuiting undefined inputs
+  const redirect = (URL) => URL && window.location.replace(URL);
+  const location = window.location.hostname;
+
+  console.log(`Window location: ${location}`);
+  // Mapping of links to redirect
+  redirect(
+    {
+      "ekd-solutions.web.app": "https://solutions.enkeldigital.com",
+      "ekd-solutions.firebaseapp.com": "https://solutions.enkeldigital.com",
+    }[location]
+  );
+})();
+
 import Vue from "vue";
 import App from "./App.vue";
 
